5e0e5e066b86633dc63eb90dc1dcfad0c0b8e549
Anel Calvo <anel_cervantes@homedepot.com>
2020-04-16 11:38:52 -0400

    1.1 update the view with styles

--- src/components /App.js
+++ src/components /App.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {StatusBar, StyleSheet, View, Text, SafeAreaView} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import reducers from '../reducers/PeopleReducer';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 const store = createStore(reducers);
 
 const App: () => React$Node = () => {
   return (
     <>
-      <Provider store={store}>
-        <StatusBar barStyle="dark-content" />
-      </Provider>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <Provider store={store}>
+          <View style={styles.body}>
+            <Text>Welcome Anel</Text>
+          </View>
+        </Provider>
+      </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: Colors.white,
+  },
+});
+
 export default App;
